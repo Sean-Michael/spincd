7e42229415d98e903bd0152f659ad9da6616d8cf
Sean-Michael <seanm.riesterer@gmail.com>
2026-04-27 16:44:46 -0700

    get single album working, ruff format, asynccontextmanager

--- main.py
+++ main.py
@@ -6,17 +6,9 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select
 from contextlib import asynccontextmanager
 
 
-class AlbumForm(BaseModel):
-    """Form input model for Albums"""
-    title: str
-    artist: str
-    release_year: int | None = None
-    genre: str | None = None
-    label: str | None = None
-
-
 class AlbumTable(SQLModel, table=True):
     """Database Table model for Albums"""
+
     id: int | None = Field(default=None, primary_key=True)
     title: str
     artist: str = Field(index=True)  # Create an index for fast Artist lookups
@@ -29,8 +21,9 @@ class AlbumTable(SQLModel, table=True):
 sqlite_file_name = "spincd.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-connect_args = {"check_same_thread": False} # Allows db access across threads
+connect_args = {"check_same_thread": False}  # Allows db access across threads
 engine = create_engine(sqlite_url, connect_args=connect_args)
+
 
 def create_db_and_tables():
     """# Create the tables for all table SQLModel models"""
@@ -42,18 +35,27 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+
 SessionDep = Annotated[Session, Depends(get_session)]
 
+
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     """Handle startup and shutdown dependencies and lifecycle of FastAPI app"""
+    # Startup
     create_db_and_tables()
     yield
+    # Shutdown
+
 
 # Initialize the FastAPI application
 app = FastAPI(lifespan=lifespan)
 
+
 @app.post("/albums")
-async def create_album(album: Annotated[AlbumTable, Form()], session: SessionDep) -> AlbumTable:
+async def create_album(
+    album: Annotated[AlbumTable, Form()], session: SessionDep
+) -> AlbumTable:
     """Create a new Album record in DB"""
     session.add(album)
     session.commit()
@@ -63,17 +65,22 @@ async def create_album(album: Annotated[AlbumTable, Form()], session: SessionDep
 
 @app.get("/albums")
 async def read_albums(
-    session: SessionDep, 
-    offset: int = 0, 
+    session: SessionDep,
+    offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-) ->list[AlbumTable]:
+) -> list[AlbumTable]:
+    """Read all the albums in the collection"""
     albums = session.exec(select(AlbumTable).offset(offset).limit(limit)).all()
     return albums
 
 
 @app.get("/albums/{album_id}")
-async def read_album_by_id(album_id):
-    return
+async def read_album_by_id(album_id: int, session: SessionDep) -> AlbumTable:
+    """Read a single album by id"""
+    album = session.get(AlbumTable, album_id)
+    if not album:
+        raise HTTPException(status_code=404, detail="Album not found")
+    return album
 
 
 @app.put("/albums/{album_id}")
